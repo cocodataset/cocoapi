b7c2fda71ca35b2bafe5fb262c5ff0c62fd5a40e
pdollar <pdollar@gmail.com>
2014-12-24 00:26:42 -0800

    gason*: adding json encoding support, removed “flatten” flag

--- gason.m
+++ gason.m
@@ -1,43 +1,45 @@
-function json = gason( varargin )
-% Parse JSON string and return corresponding JSON object.
+function out = gason( in )
+% Convert between JSON strings and corresponding JSON objects.
 %
 % This parser is based on Gason written and maintained by Ivan Vashchaev:
 %                 https://github.com/vivkin/gason
 % Gason is a "lightweight and fast JSON parser for C++". Please see the
 % above link for license information and additional details about Gason.
 %
-% This function simply calls the C++ parser and converts the output into an
-% appropriate Matlab structure. As the parsing is performed in mex the
-% resulting parser is blazingly fast. Large JSON structs (100MB+) take only
-% a few seconds to parse (compared to hours for pure Matlab parsers).
+% Given a JSON string, gason calls the C++ parser and converts the output
+% into an appropriate Matlab structure. As the parsing is performed in mex
+% the resulting parser is blazingly fast. Large JSON structs (100MB+) take
+% only a few seconds to parse (compared to hours for pure Matlab parsers).
+%
+% Given a JSON object, gason calls the C++ encoder to convert the object
+% back into a JSON string representation. Nearly any Matlab struct, cell
+% array, or numeric array represent a valid JSON object. Note that gason()
+% can be used to go both from JSON string to JSON object and back.
 %
 % Gason requires C++11 to compile (for GCC this requires version 4.7 or
 % later). The following command compiles the parser (may require tweaking):
 %   mex('CXXFLAGS="\$CXXFLAGS -std=c++11"','private/gason.cpp', ...
 %     'private/gasonMex.cpp','-output',['private/gasonMex.' mexext]);
-% Note the use of the "-std=c++11" flag. Precompiled binaries for Linux and
-% Mac are included. Please do not contact us for help with compiling.
+% Note the use of the "-std=c++11" flag. A number of precompiled binaries
+% are included, please do not contact us for help with compiling.
 %
 % Note that by default JSON arrays that contain only numbers are stored as
 % regular Matlab arrays. Likewise, JSON arrays that contain only objects of
 % the same type are stored as Matlab struct arrays. This is much faster and
 % can use considerably less memory than always using Matlab cell arrays.
-% Instead if 'flatten'=0 then all JSON arrays are always stored as cell
-% arrays (useful for compatibility with other Matlab json parsers).
 %
 % USAGE
-%  json = gason( string, [flatten] )
+%  object = gason( string )
+%  string = gason( object )
 %
-% INPUTS
-%  string     - JSON string to be parsed
-%  flatten    - [1] if 0 store all arrays as cell arrays
-%
-% OUTPUTS
-%  json       - parsed JSON object
+% INPUTS/OUTPUTS
+%  string     - JSON string
+%  object     - JSON object
 %
 % EXAMPLE
-%  s = '[{"first":"piotr","last":"dollar"},{"first":"ty","last":"lin"}]';
-%  json = gason( s )
+%  o = struct('first',{'piotr','ty'},'last',{'dollar','lin'})
+%  s = gason( o ) % convert JSON object -> JSON string
+%  p = gason( s ) % convert JSON string -> JSON object
 %
 % See also
 %
@@ -46,4 +48,4 @@ function json = gason( varargin )
 % Code written by Piotr Dollar and Tsung-Yi Lin, 2014.
 % Licensed under the Simplified BSD License [see private/bsd.txt]
 
-json = gasonMex( varargin{:} );
+out = gasonMex( in );
--- private/gasonMex.cpp
+++ private/gasonMex.cpp
@@ -7,6 +7,10 @@
 #include "gason.h"
 #include "mex.h"
 #include "string.h"
+#include <cstdint>
+#include <iomanip>
+#include <sstream>
+typedef std::ostringstream ostrm;
 
 bool isRegularObjArray( const JsonValue &a ) {
   // check if all JSON_OBJECTs in JSON_ARRAY have the same fields
@@ -20,7 +24,7 @@ bool isRegularObjArray( const JsonValue &a ) {
   delete [] keys; return true;
 }
 
-mxArray* toMatlab( const JsonValue &o, bool flatten ) {
+mxArray* json( const JsonValue &o ) {
   // convert JsonValue to Matlab mxArray
   int k, m, n; mxArray *M; const char **keys;
   switch( o.getTag() ) {
@@ -31,21 +35,21 @@ mxArray* toMatlab( const JsonValue &o, bool flatten ) {
     case JSON_ARRAY: {
       if(!o.toNode()) return mxCreateCellMatrix(1,0);
       JsonValue o0=o.toNode()->value; JsonTag tag=o0.getTag();
-      n=0; for(auto i:o) n++; bool isRegular=flatten;
+      n=0; for(auto i:o) n++; bool isRegular=true;
       for(auto i:o) isRegular=isRegular && i->value.getTag()==tag;
       if( isRegular && tag==JSON_OBJECT && isRegularObjArray(o) ) {
         m=0; for(auto j:o0) m++; keys=new const char*[m];
         k=0; for(auto j:o0) keys[k++]=j->key;
         M = mxCreateStructMatrix(1,n,m,keys);
         k=0; for(auto i:o) { m=0; for(auto j:i->value)
-          mxSetFieldByNumber(M,k,m++,toMatlab(j->value,flatten)); k++; }
+          mxSetFieldByNumber(M,k,m++,json(j->value)); k++; }
         delete [] keys; return M;
       } else if( isRegular && tag==JSON_NUMBER ) {
         M = mxCreateDoubleMatrix(1,n,mxREAL); double *p=mxGetPr(M);
         k=0; for(auto i:o) p[k++]=i->value.toNumber(); return M;
       } else {
         M = mxCreateCellMatrix(1,n);
-        k=0; for(auto i:o) mxSetCell(M,k++,toMatlab(i->value,flatten));
+        k=0; for(auto i:o) mxSetCell(M,k++,json(i->value));
         return M;
       }
     }
@@ -54,7 +58,7 @@ mxArray* toMatlab( const JsonValue &o, bool flatten ) {
       n=0; for(auto i:o) n++; keys=new const char*[n];
       k=0; for(auto i:o) keys[k++]=i->key;
       M = mxCreateStructMatrix(1,1,n,keys); k=0;
-      for(auto i:o) mxSetFieldByNumber(M,0,k++,toMatlab(i->value,flatten));
+      for(auto i:o) mxSetFieldByNumber(M,0,k++,json(i->value));
       delete [] keys; return M;
     case JSON_TRUE:
       return mxCreateDoubleScalar(1);
@@ -65,21 +69,79 @@ mxArray* toMatlab( const JsonValue &o, bool flatten ) {
   }
 }
 
-// json = mexFunction( jsonstring, [flatten] )
+template<class T, class C> ostrm& json( ostrm &S, T *A, int n ) {
+  // convert numeric array to JSON string with casting
+  if(n==0) { S<<"[]"; return S; } if(n==1) { S<<C(A[0]); return S; }
+  S<<"["; for(int i=0; i<n-1; i++) S<<C(A[i])<<", ";
+  S<<C(A[n-1]); S<<"]"; return S;
+}
+
+template<class T> ostrm& json( ostrm &S, T *A, int n ) {
+  // convert numeric array to JSON string without casting
+  return json<T,T>(S,A,n);
+}
+
+ostrm& json( ostrm &S, const char *A ) {
+  // convert char array to JSON string (handle escape characters)
+  #define RPL(a,b) case a: { S << b; A++; break; }
+  S << "\""; while( *A>0 ) switch( *A ) {
+    RPL('"',"\\\""); RPL('\\',"\\\\"); RPL('/',"\\/"); RPL('\b',"\\b");
+    RPL('\f',"\\f"); RPL('\n',"\\n"); RPL('\r',"\\r"); RPL('\t',"\\t");
+    default: S << *A; A++;
+  }
+  S << "\""; return S;
+}
+
+ostrm& json( ostrm& S, const mxArray *M ) {
+  // convert Matlab mxArray to JSON string
+  int i, j, m, n=mxGetNumberOfElements(M);
+  void *A=mxGetData(M); ostrm *nms;
+  switch( mxGetClassID(M) ) {
+    case mxDOUBLE_CLASS:  return json(S,(double*)   A,n);
+    case mxSINGLE_CLASS:  return json(S,(float*)    A,n);
+    case mxINT64_CLASS:   return json(S,(int64_t*)  A,n);
+    case mxUINT64_CLASS:  return json(S,(uint64_t*) A,n);
+    case mxINT32_CLASS:   return json(S,(int32_t*)  A,n);
+    case mxUINT32_CLASS:  return json(S,(uint32_t*) A,n);
+    case mxINT16_CLASS:   return json(S,(int16_t*)  A,n);
+    case mxUINT16_CLASS:  return json(S,(uint16_t*) A,n);
+    case mxINT8_CLASS:    return json<int8_t,int32_t>(S,(int8_t*) A,n);
+    case mxUINT8_CLASS:   return json<uint8_t,uint32_t>(S,(uint8_t*) A,n);
+    case mxLOGICAL_CLASS: return json<uint8_t,uint32_t>(S,(uint8_t*) A,n);
+    case mxCHAR_CLASS:    return json(S,mxArrayToString(M));
+    case mxCELL_CLASS:
+      S << "["; for(i=0; i<n-1; i++) json(S,mxGetCell(M,i)) << ", ";
+      if(n>0) json(S,mxGetCell(M,n-1)); S << "]"; return S;
+    case mxSTRUCT_CLASS:
+      if(n==0) { S<<"{}"; return S; } m=mxGetNumberOfFields(M);
+      if(m==0) { S<<"["; for(i=0; i<n; i++) S<<"{},"; S<<"]"; return S; }
+      if(n>1) S<<"["; nms=new ostrm[m];
+      for(j=0; j<m; j++) json(nms[j],mxGetFieldNameByNumber(M,j));
+      for(i=0; i<n; i++) for(j=0; j<m; j++) {
+        if(j==0) S << "{"; S << nms[j].str() << ": ";
+        json(S,mxGetFieldByNumber(M,i,j)) << ((j<m-1) ? ", " : "}");
+        if(j==m-1 && i<n-1) S<<", ";
+      }
+      if(n>1) S<<"]"; delete [] nms; return S;
+    default:
+      mexErrMsgTxt( "Unknown type." ); return S;
+  }
+}
+
 void mexFunction( int nl, mxArray *pl[], int nr, const mxArray *pr[] )
 {
-  // get inputs
-  if( nr<1 && nr>2 ) mexErrMsgTxt("One or two inputs expected.");
+  if( nr!=1 ) mexErrMsgTxt("One input expected.");
   if( nl>1 ) mexErrMsgTxt("One output expected.");
-  char *str = mxArrayToString(pr[0]);
-  bool flatten = (nr>1) ? mxGetScalar(pr[1])>0 : 1;
-  
-  // run gason parser
-  char *endptr; JsonValue value; JsonAllocator allocator;
-  int status = jsonParse(str, &endptr, &value, allocator);
-  if( status != JSON_OK) mexErrMsgTxt(jsonStrError(status));
-  
-  // convert to matlab struct and free str
-  pl[0] = toMatlab( value, flatten );
-  mxFree(str);
+  if( mxGetClassID(pr[0])==mxCHAR_CLASS ) {
+    // object = mexFunction( string )
+    char *str = mxArrayToString(pr[0]);
+    char *endptr; JsonValue value; JsonAllocator allocator;
+    int status = jsonParse(str, &endptr, &value, allocator);
+    if( status != JSON_OK) mexErrMsgTxt(jsonStrError(status));
+    pl[0] = json( value ); mxFree(str);
+  } else {
+    // string = mexFunction( object )
+    ostrm S; S << std::setprecision(10); json(S,pr[0]);
+    pl[0] = mxCreateString( S.str().c_str() );
+  }
 }
