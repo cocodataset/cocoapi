14f01374264d45297370e676729be2583cc4dacb
pdollar <pdollar@gmail.com>
2015-01-25 11:46:04 -0800

    updated demo for new data location (and works if no caption annotations)

--- MatlabAPI/cocoDemo.m
+++ MatlabAPI/cocoDemo.m
@@ -1,7 +1,7 @@
 %% Demo for the CocoApi (see CocoApi.m)
 
 %% initialize COCO api for instance annotations
-dataDir='.'; dataType='val2014';
+dataDir='../'; dataType='val2014';
 annFile=sprintf('%s/annotations/instances_%s.json',dataDir,dataType);
 if(~exist('coco','var')), coco=CocoApi(annFile); end
 
@@ -28,6 +28,7 @@ anns = coco.loadAnns(annIds); coco.showAnns(anns);
 
 %% initialize COCO api for caption annotations
 annFile=sprintf('%s/annotations/captions_%s.json',dataDir,dataType);
+if(~exist(annFile,'file')), return; end
 if(~exist('caps','var')), caps=CocoApi(annFile); end
 
 %% load and display caption annotations
