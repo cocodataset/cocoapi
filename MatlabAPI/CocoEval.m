04f15b7cfb857fdc01d9901b3b358228a5937622
pdollar <pdollar@gmail.com>
2015-07-01 16:16:12 -0700

    CocoEval.m: (refactoring) split evaluateImg() into separate function, fixed comment indent

--- MatlabAPI/CocoEval.m
+++ MatlabAPI/CocoEval.m
@@ -2,13 +2,13 @@ classdef CocoEval < handle
   % Interface for evaluating detection on the Microsoft COCO dataset.
   %
   % The usage for CocoEval is as follows:
-  %   cocoGt=..., cocoDt=...        % load dataset and results
-  %   E = CocoEval(cocoGt,cocoDt);  % initialize CocoEval object
-  %   E.params.recThrs = ...;       % set parameters as desired
-  %   E.evaluate();                 % run per image evaluation
-  %   disp( E.evalImgs )            % inspect per image results
-  %   E.accumulate();               % accumulate per image results
-  %   disp( E.eval )                % inspect accumulated results
+  %  cocoGt=..., cocoDt=...       % load dataset and results
+  %  E = CocoEval(cocoGt,cocoDt); % initialize CocoEval object
+  %  E.params.recThrs = ...;      % set parameters as desired
+  %  E.evaluate();                % run per image evaluation
+  %  disp( E.evalImgs )           % inspect per image results
+  %  E.accumulate();              % accumulate per image results
+  %  disp( E.eval )               % inspect accumulated results
   % For example usage see evalDemo.m and http://mscoco.org/.
   %
   % The evaluation parameters are as follows (defaults in brackets):
@@ -25,26 +25,26 @@ classdef CocoEval < handle
   %
   % evaluate(): evaluates detections on every image and every category and
   % concats the ~N*K results into the struct array "evalImgs" with fields:
-  %   imgId     - results for the img with the given id
-  %   catId     - results for the cat with the given id
-  %   dtIds     - [1xD] id for each of the D detections (dt)
-  %   gtIds     - [1xG] id for each of the G ground truths (gt)
-  %   dtMatches - [TxD] matching gt id at each IoU or 0
-  %   gtMatches - [TxG] matching dt id at each IoU or 0
-  %   dtScores  - [1xD] confidence of each dt
-  %   gtIgnore  - [1xG] ignore flag for each gt
-  %   dtIgnore  - [TxD] ignore flag for each dt at each IoU
-  %   ious      - [DxG] iou between every dt and gt
+  %  imgId      - results for the img with the given id
+  %  catId      - results for the cat with the given id
+  %  dtIds      - [1xD] id for each of the D detections (dt)
+  %  gtIds      - [1xG] id for each of the G ground truths (gt)
+  %  dtMatches  - [TxD] matching gt id at each IoU or 0
+  %  gtMatches  - [TxG] matching dt id at each IoU or 0
+  %  dtScores   - [1xD] confidence of each dt
+  %  gtIgnore   - [1xG] ignore flag for each gt
+  %  dtIgnore   - [TxD] ignore flag for each dt at each IoU
+  %  ious       - [DxG] iou between every dt and gt
   %
   % accumulate(): accumulates the per-image, per-category evaluation
   % results in "evalImgs" into the struct "eval" with fields:
-  %   params    - parameters used for evaluation
-  %   date      - date evaluation was performed
-  %   counts    - [T,R,K] counts for ious/recalls/categories
-  %   precision - [TxRxK] precision for every iou/recall/category
-  %   recall    - [TxK] maximum recall for every iou/category
-  %   ap        - average precision across all T*R*K precision values
-  %   ar        - average recall across all T*K recall values
+  %  params     - parameters used for evaluation
+  %  date       - date evaluation was performed
+  %  counts     - [T,R,K] counts for ious/recalls/categories
+  %  precision  - [TxRxK] precision for every iou/recall/category
+  %  recall     - [TxK] maximum recall for every iou/category
+  %  ap         - average precision across all T*R*K precision values
+  %  ar         - average recall across all T*K recall values
   % Note: categories with no instances are removed (K may decrease).
   %
   % See also CocoApi, MaskApi, cocoDemo, evalDemo
@@ -96,10 +96,6 @@ classdef CocoEval < handle
         for j=1:length(dk), nDts(dk(j))=nDts(dk(j))+1; end
         [~,gk]=sort(gk); gts=gts(gk); nGtsc=cumsum([0; nGts(:)]);
         [~,dk]=sort(dk); dts=dts(dk); nDtsc=cumsum([0; nDts(:)]);
-        % set ignore flags for gt
-        i=[gts.iscrowd]; if(isfield(gts,'ignore')), i=i|[gts.ignore]; end
-        if(~all(aRng==[0 inf])), a=[gts.area]; i=i|a<aRng(1)|a>aRng(2); end
-        gts().ignore=[]; for g=1:length(gts), gts(g).ignore=i(g); end
         for i=find(nGts>0 | nDts>0)'
           % perform evaluation for single category and image
           gt=gts(nGtsc(i)+1:nGtsc(i+1)); G=length(gt);
@@ -110,45 +106,8 @@ classdef CocoEval < handle
             for g=1:G, o=gt(g).segmentation; if(~isstruct(o)),
                 gt(g).segmentation=MaskApi.frPoly(o,t(1),t(2)); end; end
           end
-          % sort dt highest score first, sort gt ignore last
-          [~,o]=sort([gt.ignore],'ascend'); gt=gt(o);
-          [~,o]=sort([dt.score],'descend'); dt=dt(o);
-          if(D>p.maxDets), D=p.maxDets; dt=dt(1:D); end
-          % compute iou between each dt and gt region
-          iscrowd = uint8([gt.iscrowd]); t=p.useSegm;
-          if(t), g=[gt.segmentation]; else g=reshape([gt.bbox],4,[])'; end
-          if(t), d=[dt.segmentation]; else d=reshape([dt.bbox],4,[])'; end
-          ious = MaskApi.iou(d,g,iscrowd);
-          % attempt to match each (sorted) dt to each (sorted) gt
-          gtm=zeros(T,G); gtIds=[gt.id]; gtIg=[gt.ignore];
-          dtm=zeros(T,D); dtIds=[dt.id]; dtIg=zeros(T,D);
-          for t=1:T
-            for d=1:D
-              % information about best match so far (m=0 -> unmatched)
-              iou=min(p.iouThrs(t),1-1e-10); m=0;
-              for g=1:G
-                % if this gt already matched, and not a crowd, continue
-                if( gtm(t,g)>0 && ~iscrowd(g) ), continue; end
-                % if dt matched to reg gt, and on ignore gt, stop
-                if( m>0 && gtIg(m)==0 && gtIg(g)==1 ), break; end
-                % continue to next gt unless better match made
-                if( ious(d,g) < iou ), continue; end
-                % match successful and best so far, store appropriately
-                iou=ious(d,g); m=g;
-              end
-              % if match made store id of match for both dt and gt
-              if(~m), continue; end; dtIg(t,d)=gtIg(m);
-              dtm(t,d)=gtIds(m); gtm(t,m)=dtIds(d);
-            end
-          end
-          % set unmatched detections outside of area range to ignore
-          if(~all(aRng==[0 inf]) && ~isempty(dt)), a=[dt.area];
-            dtIg = dtIg | (dtm==0 & repmat(a<aRng(1)|a>aRng(2),T,1)); end
-          % store results for given image and category
-          ev.evalImgs{i,k}=struct('imgId',p.imgIds(i),...
-            'catId',p.catIds(k),'dtIds',dtIds,'gtIds',gtIds,...
-            'dtMatches',dtm,'gtMatches',gtm,'dtScores',[dt.score],...
-            'gtIgnore',gtIg,'dtIgnore',dtIg,'ious',ious);
+          q=p; q.catIds=p.catIds(k); q.imgIds=p.imgIds(i);
+          ev.evalImgs{i,k}=CocoEval.evaluateImg(gt,dt,q);
         end
       end
       ev.evalImgs=[ev.evalImgs{:}];
@@ -186,4 +145,49 @@ classdef CocoEval < handle
     end
   end
   
+  methods( Static )
+    function e = evaluateImg( gt, dt, params )
+      % Run evaluation for a single image and category.
+      p=params; T=length(p.iouThrs); aRng=p.areaRng;
+      if(~isfield(gt,'ignore')), [gt(:).ignore]=deal(0); end
+      a=[gt.area]; gtIg=[gt.iscrowd]|[gt.ignore]|a<aRng(1)|a>aRng(2);
+      G=length(gt); D=length(dt); for g=1:G, gt(g).ignore=gtIg(g); end
+      % sort dt highest score first, sort gt ignore last
+      [~,o]=sort([gt.ignore],'ascend'); gt=gt(o);
+      [~,o]=sort([dt.score],'descend'); dt=dt(o);
+      if(D>p.maxDets), D=p.maxDets; dt=dt(1:D); end
+      % compute iou between each dt and gt region
+      iscrowd = uint8([gt.iscrowd]); t=p.useSegm;
+      if(t), g=[gt.segmentation]; else g=reshape([gt.bbox],4,[])'; end
+      if(t), d=[dt.segmentation]; else d=reshape([dt.bbox],4,[])'; end
+      ious=MaskApi.iou(d,g,iscrowd);
+      % attempt to match each (sorted) dt to each (sorted) gt
+      gtm=zeros(T,G); gtIds=[gt.id]; gtIg=[gt.ignore];
+      dtm=zeros(T,D); dtIds=[dt.id]; dtIg=zeros(T,D);
+      for t=1:T
+        for d=1:D
+          % information about best match so far (m=0 -> unmatched)
+          iou=min(p.iouThrs(t),1-1e-10); m=0;
+          for g=1:G
+            % if this gt already matched, and not a crowd, continue
+            if( gtm(t,g)>0 && ~iscrowd(g) ), continue; end
+            % if dt matched to reg gt, and on ignore gt, stop
+            if( m>0 && gtIg(m)==0 && gtIg(g)==1 ), break; end
+            % if match successful and best so far, store appropriately
+            if( ious(d,g)>=iou ), iou=ious(d,g); m=g; end
+          end
+          % if match made store id of match for both dt and gt
+          if(~m), continue; end; dtIg(t,d)=gtIg(m);
+          dtm(t,d)=gtIds(m); gtm(t,m)=dtIds(d);
+        end
+      end
+      % set unmatched detections outside of area range to ignore
+      if(isempty(dt)), a=zeros(1,0); else a=[dt.area]; end
+      dtIg = dtIg | (dtm==0 & repmat(a<aRng(1)|a>aRng(2),T,1));
+      % store results for given image and category
+      e=struct('imgId',p.imgIds,'catId',p.catIds,'dtIds',dtIds,'gtIds',...
+        gtIds,'dtMatches',dtm,'gtMatches',gtm,'dtScores',[dt.score],...
+        'gtIgnore',gtIg,'dtIgnore',dtIg,'ious',ious);
+    end
+  end
 end
