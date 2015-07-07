fce54cb84fd4548a0ab7c69ef83e73eee0ecb363
pdollar <pdollar@gmail.com>
2015-07-07 08:51:18 -0700

    CocoUtils.m->convertPascalGt(): tiny fix when converting PASCAL->COCO format

--- MatlabAPI/CocoUtils.m
+++ MatlabAPI/CocoUtils.m
@@ -42,9 +42,9 @@ classdef CocoUtils
       f=fopen([dev '/VOC' year '/ImageSets/Main/' split '.txt']);
       is=textscan(f,'%s %*s'); is=is{1}; fclose(f); n=length(is);
       data=CocoUtils.initData(C,n);
-      for i=1:n
+      for i=1:n, nm=[is{i} '.jpg'];
         f=[dev '/VOC' year '/Annotations/' is{i} '.xml'];
-        R=PASreadrecord(f); hw=R.imgsize; O=R.objects; nm=[is{i} '.jpg'];
+        R=PASreadrecord(f); hw=R.imgsize([2 1]); O=R.objects;
         id=str2double(is{i}); ignore=[O.difficult]; bbs=cat(1,O.bbox);
         t=catsMap.values({O.class}); catIds=[t{:}]; iscrowd=ignore*0;
         data=CocoUtils.addData(data,nm,id,hw,catIds,ignore,iscrowd,bbs);
