111e0bf04e3f9e22a61c308d917da9915ed07c98
tsungyi <tsungyilin87@gmail.com>
2015-07-09 10:24:46 -0700

    add c99 flag for compilation

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,14 +1,24 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+from distutils.extension import Extension
 import numpy as np
 
-# To compile run "python setup.py build_ext --inplace"
+# To compile and install locally run "python setup.py build_ext --inplace"
+# To install library to Python site-packages run "python setup.py build_ext install"
+
+ext_modules = [
+    Extension(
+        'pycocotools._mask',
+        sources=['../MatlabAPI/private/maskApi.c', 'pycocotools/_mask.pyx'],
+        include_dirs = [np.get_include(), '../MatlabAPI/private'],
+        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+    )
+]
 
 setup(name='pycocotools',
       packages=['pycocotools'],
       package_dir = {'pycocotools': 'pycocotools'},
       version='1.0.2',
-      include_dirs = [np.get_include(), '../MatlabAPI/private'],
       ext_modules=
-        cythonize('pycocotools/_mask.pyx'),
+          cythonize(ext_modules)
       )
