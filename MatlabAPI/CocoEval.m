79d936c1339fbfda326d9c0da62cc1796d079430
pdollar <pdollar@gmail.com>
2015-11-28 10:21:33 -0800

    CocoEval.m->analyze() — added per-area analysis, refactored, optimized

--- MatlabAPI/CocoEval.m
+++ MatlabAPI/CocoEval.m
@@ -260,59 +260,66 @@ classdef CocoEval < handle
     function analyze( ev )
       % Derek Hoiem style analyis of false positives.
       %  Preliminary implementation, undocumented. Use at your own risk.
+      %  If pdfcrop not found then set pdfcrop path appropriately:
+      %  setenv('PATH',[getenv('PATH') ':/Library/TeX/texbin/']);
+      outDir='./analyze'; if(~exist(outDir,'dir')), mkdir(outDir); end
       if(~isfield(ev.cocoGt.data.annotations,'ignore')),
         [ev.cocoGt.data.annotations.ignore]=deal(0); end
-      ev.params.maxDets=100; ev.params.areaRng=[0 1e10];
-      ev.params.useCats=0; cocoDtAll=ev.cocoDt;
-      if(~exist('./analyze','dir')), mkdir('analyze'); end
-      catIds=ev.cocoGt.getCatIds(); K=length(catIds);
-      rs=ev.params.recThrs; ps=zeros(7,length(rs),K);
-      for k=1:K, catId=catIds(k);
+      dt=ev.cocoDt; gt=ev.cocoGt; prm=ev.params; rs=prm.recThrs;
+      ev.params.maxDets=100; catIds=ev.cocoGt.getCatIds();
+      % compute precision at different IoU values
+      ev.params.catIds=catIds; ev.params.iouThrs=[.75 .5 .1];
+      ev.evaluate(); ev.accumulate(); ps=ev.eval.precision;
+      ps(4:7,:,:,:)=0; ev.params.iouThrs=.1; ev.params.useCats=0;
+      for k=1:length(catIds), catId=catIds(k);
         nm=ev.cocoGt.loadCats(catId); nm=[nm.supercategory '-' nm.name];
-        fprintf('Analyzing %s...\n',nm); clk=clock;
-        dt=ev.cocoDt.data.annotations; dt=dt([dt.category_id]==catId);
-        D=ev.cocoGt.data; D.annotations=dt; ev.cocoDt=CocoApi(D);
-        ev.params.iouThrs=[.75 .5 .1]; ps(1:3,:,k)=precision(ev,0,catId);
-        ev.params.iouThrs=.1; ps(4,:,k)=precision(ev,1,catId);
-        ps(5,:,k)=precision(ev,2,catId); ev.cocoDt=cocoDtAll;
-        ps(6,:,k)=ps(5,:,k)>0; ps(7,:,k)=1; makeplot(rs,ps(:,:,k),nm);
-        fprintf('DONE (t=%0.2fs).\n\n',etime(clock,clk));
+        fprintf('\nAnalyzing %s...\n',nm); clk=clock;
+        % select detections for single category only
+        D=dt.data; A=D.annotations; A=A([A.category_id]==catId);
+        D.annotations=A; ev.cocoDt=dt; ev.cocoDt=CocoApi(D);
+        % compute precision but ignore superclass confusion
+        is=gt.getCatIds('supNms',gt.loadCats(catId).supercategory);
+        D=gt.data; A=D.annotations; A=A(ismember([A.category_id],is));
+        [A([A.category_id]~=catId).ignore]=deal(1);
+        D.annotations=A; ev.cocoGt=CocoApi(D);
+        ev.evaluate(); ev.accumulate(); ps(4,:,k,:)=ev.eval.precision;
+        % compute precision but ignore any class confusion
+        D=gt.data; A=D.annotations;
+        [A([A.category_id]~=catId).ignore]=deal(1);
+        D.annotations=A; ev.cocoGt=gt; ev.cocoGt.data=D;
+        ev.evaluate(); ev.accumulate(); ps(5,:,k,:)=ev.eval.precision;
+        % fill in background and false negative errors and plot
+        ps(ps==-1)=0; ps(6,:,k,:)=ps(5,:,k,:)>0; ps(7,:,k,:)=1;
+        makeplot(rs,ps(:,:,k,:),outDir,nm);
+        fprintf('DONE (t=%0.2fs).\n',etime(clock,clk));
       end
-      makeplot(rs,mean(ps,3),'overall');
-      sup={ev.cocoGt.data.categories.supercategory};
-      for k=unique(sup), ps1=mean(ps(:,:,strcmp(sup,k)),3);
-        makeplot(rs,ps1,k{1}); end
+      % plot averages over all categories and supercategories
+      ev.cocoDt=dt; ev.cocoGt=gt; ev.params=prm;
+      fprintf('\n'); makeplot(rs,mean(ps,3),outDir,'overall-all');
+      sup={ev.cocoGt.loadCats(catIds).supercategory};
+      for k=unique(sup), ps1=mean(ps(:,:,strcmp(sup,k),:),3);
+        makeplot(rs,ps1,outDir,['overall-' k{1}]); end
       
-      function p = precision( ev, type, catId )
-        % Helper that computes different types of precision.
-        gt=ev.cocoGt; gtOrig=gt; ann=gt.data.annotations;
-        if( type==0 )
-          ann=ann([ann.category_id]==catId);
-          data=gt.data; data.annotations=ann; gt=CocoApi(data);
-        elseif( type==1 )
-          is=gt.getCatIds('supNms',gt.loadCats(catId).supercategory);
-          ann=ann(ismember([ann.category_id],is));
-          [ann([ann.category_id]~=catId).ignore]=deal(1);
-          data=gt.data; data.annotations=ann; gt=CocoApi(data);
-        end
-        [gt.data.annotations(gt.inds.annCatIds~=catId).ignore]=deal(1);
-        ev.cocoGt=gt; ev.evaluate(); ev.accumulate();
-        p=ev.eval.precision; ev.cocoGt=gtOrig;
-      end
-      
-      function makeplot( rs, ps, nm )
-        % Plot FP breakdown using area plot (optionally pdfcrop).
+      function makeplot( rs, ps, outDir, nm )
+        % Plot FP breakdown using area plot.
+        fprintf('Plotting results...                  '); t=clock;
         cs=[ones(2,3); .31 .51 .74; .75 .31 .30;
           .36 .90 .38; .50 .39 .64; 1 .6 0]; m=size(ps,1);
-        ap=round(mean(ps,2)*1000); ds=[ps(1,:); diff(ps)]';
-        ls={'C75','C50','Loc','Sim','Oth','BG','[1.00] FN'};
-        for i=1:m-1, ls{i}=sprintf('[.%3i] %s',ap(i),ls{i}); end
-        figure(1); h=area(rs,ds); title(nm); legend(ls,'location','sw');
-        for i=1:m, set(h(i),'FaceColor',cs(i,:)); end
-        xlabel('recall'); ylabel('precision'); set(gca,'fontsize',20)
-        nm=['analyze/' regexprep(nm,' ','_')]; print(nm,'-dpdf')
-        if(0), setenv('PATH',[getenv('PATH') ':/Library/TeX/texbin/']); end
-        if(0), system(['pdfcrop ' nm '.pdf ' nm '.pdf']); end
+        areaNms={'all','small','medium','large'}; nm0=nm; ps0=ps;
+        for a=1:size(ps,4)
+          nm=[nm0 '-' areaNms{a}]; ps=ps0(:,:,:,a);
+          ap=round(mean(ps,2)*1000); ds=[ps(1,:); diff(ps)]';
+          ls={'C75','C50','Loc','Sim','Oth','BG','FN'};
+          for i=1:m, if(ap(i)==1000), ls{i}=['[1.00] ' ls{i}]; else
+              ls{i}=sprintf('[.%03i] %s',ap(i),ls{i}); end; end
+          figure(1); h=area(rs,ds); legend(ls,'location','sw');
+          for i=1:m, set(h(i),'FaceColor',cs(i,:)); end; title(nm)
+          xlabel('recall'); ylabel('precision'); set(gca,'fontsize',20)
+          nm=[outDir '/' regexprep(nm,' ','_')]; print(nm,'-dpdf')
+          [status,~]=system(['pdfcrop ' nm '.pdf ' nm '.pdf']);
+          if(status>0), warning('pdfcrop not found.'); end
+        end
+        fprintf('DONE (t=%0.2fs).\n',etime(clock,t));
       end
     end
   end
