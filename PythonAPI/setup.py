057ecf24c34b67483a93798da202be93dfa23c84
Phil Ferriere <pferriere@hotmail.com>
2017-11-01 23:45:17 -0700

    Fix Windows build

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -11,7 +11,7 @@ ext_modules = [
         'pycocotools._mask',
         sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
         include_dirs = [np.get_include(), '../common'],
-        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        extra_compile_args=[] # Phil: originally was ['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
     )
 ]
 
