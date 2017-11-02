ac776d321197f2fc6c30e1489fb687cdc7604090
Phil Ferriere <pferriere@hotmail.com>
2017-11-02 10:15:59 -0700

    Document changes to setup procedure

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -3,15 +3,17 @@ from Cython.Build import cythonize
 from distutils.extension import Extension
 import numpy as np
 
-# To compile and install locally run "python setup.py build_ext --inplace"
-# To install library to Python site-packages run "python setup.py build_ext install"
+# To install and compile to your anaconda/python site-packages, simply run:
+# $ pip install git+https://github.com/philferriere/cocoapi.git#egg=pycocotools^&subdirectory=PythonAPI
+# Note that the original compile flags below are GCC flags unsupported by the Visual C++ 2015 build tools.
+# They can safely be removed.
 
 ext_modules = [
     Extension(
         'pycocotools._mask',
         sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
         include_dirs = [np.get_include(), '../common'],
-        extra_compile_args=[] # Phil: originally was ['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        extra_compile_args=[] # originally was ['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
     )
 ]
 
