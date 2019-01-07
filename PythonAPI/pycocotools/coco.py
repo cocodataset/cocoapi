5f0eb7d32aad190b908edc9d3b838ac31b397d66
Ka Wai Tsoi <kawaitsoi@Kas-MacBook-Pro.local>
2019-01-07 09:59:05 -0800

    exclude string type from _isArrayLike test

--- PythonAPI/pycocotools/coco.py
+++ PythonAPI/pycocotools/coco.py
@@ -64,7 +64,7 @@ elif PYTHON_VERSION == 3:
 
 
 def _isArrayLike(obj):
-    return hasattr(obj, '__iter__') and hasattr(obj, '__len__')
+    return hasattr(obj, '__iter__') and hasattr(obj, '__len__') and type(obj).__name__!="str"
 
 
 class COCO:
