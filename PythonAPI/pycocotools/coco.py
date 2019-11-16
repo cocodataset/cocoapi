13a00ad3c90b7c50f740a2777a2cfa029609fd4b
AlphaGoLab <alphagomk@foxmail.com>
2019-11-16 21:51:39 +0800

    update coco.py

--- PythonAPI/pycocotools/coco.py
+++ PythonAPI/pycocotools/coco.py
@@ -234,7 +234,7 @@ class COCO:
         """
         show bounding box of annotations or predictions
         anns: loadAnns() annotations or predictions subject to coco results format
-        label_box: show label text's bounding box or not
+        label_box: show background of category labels or not
         """
         if len(anns) == 0:
             return 0
@@ -242,8 +242,11 @@ class COCO:
         ax.set_autoscale_on(False)
         polygons = []
         color = []
+        image2color=dict()
+        for cat in self.getCatIds():
+            image2color[cat]=(np.random.random((1, 3))*0.7+0.3).tolist()[0]
         for ann in anns:
-            c = (np.random.random((1, 3))*0.6+0.4).tolist()[0]  # color
+            c=image2color[ann['category_id']]
             [bbox_x, bbox_y, bbox_w, bbox_h] = ann['bbox']
             poly = [[bbox_x, bbox_y], [bbox_x, bbox_y+bbox_h], [bbox_x+bbox_w, bbox_y+bbox_h], [bbox_x+bbox_w, bbox_y]]
             np_poly = np.array(poly).reshape((4,2))
@@ -259,10 +262,12 @@ class COCO:
                 ax.text(bbox_x, bbox_y, '%s: %.2f'%(self.loadCats(ann['category_id'])[0]['name'], ann['score']), color='white', bbox=label_bbox)
             else:
                 ax.text(bbox_x, bbox_y, '%s'%(self.loadCats(ann['category_id'])[0]['name']), color='white', bbox=label_bbox)
-        p = PatchCollection(polygons, facecolor=color, linewidths=0, alpha=0.4)
-        ax.add_collection(p)
+        # option for filling bounding box
+        # p = PatchCollection(polygons, facecolor=color, linewidths=0, alpha=0.4)
+        # ax.add_collection(p)
         p = PatchCollection(polygons, facecolor='none', edgecolors=color, linewidths=2)
         ax.add_collection(p)
+        
 
     def showAnns(self, anns):
         """
