d234b096b49a578c490ff5ff6dcec46072bdf68a
Jerry Jiarui XU <xvjiarui0826@gmail.com>
2020-05-24 23:07:12 +0800

    add coco alias func (#1)
    * add coco alias func
    
    * delete MatLab. Lua
    
    * add pre-commit hook
    
    * remove build flag
    
    * change to github action
    
    * remove action
    
    * re-run precommit
    
    * fixed action
    
    * fixed action
    
    * fixed action

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,27 +1,28 @@
-from setuptools import setup, Extension
 import numpy as np
+from setuptools import Extension, setup
 
 # To compile and install locally run "python setup.py build_ext --inplace"
-# To install library to Python site-packages run "python setup.py build_ext install"
+# To install library to Python site-packages run
+# "python setup.py build_ext install"
+# Note that the original compile flags below are GCC flags unsupported by
+# the Visual C++ 2015 build tools.
+# They can safely be removed.
 
 ext_modules = [
     Extension(
         'pycocotools._mask',
         sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
-        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        include_dirs=[np.get_include(), '../common'],
+        # extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        extra_compile_args=[],
     )
 ]
 
-setup(
-    name='pycocotools',
-    packages=['pycocotools'],
-    package_dir = {'pycocotools': 'pycocotools'},
-    install_requires=[
-        'setuptools>=18.0',
-        'cython>=0.27.3',
-        'matplotlib>=2.1.0'
-    ],
-    version='2.0',
-    ext_modules= ext_modules
-)
+setup(name='pycocotools',
+      packages=['pycocotools'],
+      package_dir={'pycocotools': 'pycocotools'},
+      install_requires=[
+          'setuptools>=18.0', 'cython>=0.27.3', 'matplotlib>=2.1.0'
+      ],
+      version='2.0',
+      ext_modules=ext_modules)
