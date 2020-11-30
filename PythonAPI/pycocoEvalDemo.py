03d67c4e58d4af64b8d756af10bc35984044d6cf
dh <jinmingteo95@gmail.com>
2020-11-30 12:58:52 +0800

    [IO IMPROVMENT] added argparse and cleaned up code

--- PythonAPI/pycocoEvalDemo.py
+++ PythonAPI/pycocoEvalDemo.py
@@ -1,21 +1,27 @@
-import matplotlib.pyplot as plt
+import argparse
+from pathlib import Path
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
-import numpy as np
-import skimage.io as io
+
+parser = argparse.ArgumentParser()
+parser.add_argument('GTfile')
+parser.add_argument('DTfile')
+args = parser.parse_args()
+
+annFile = Path(args.GTfile)
+resFile = Path(args.DTfile)
+assert annFile.is_file()
+assert resFile.is_file()
 
 annType = ['segm','bbox','keypoints', 'obb']
-annType = 'obb'      #specify type here
-prefix = 'person_keypoints' if annType=='keypoints' else 'instances'
+annType = 'obb'      # specify type here
 print ('Running demo for *%s* results.'%(annType))
 
 #initialize COCO ground truth api
-annFile = 'test.json'
 cocoGt=COCO(annFile)
 
 #initialize COCO detections api
-resFile='filtered_coco.json'
-cocoDt=cocoGt.loadRes(resFile)
+cocoDt=cocoGt.loadRes(str(resFile))
 
 imgIds=sorted(cocoGt.getImgIds())
 imgIds=imgIds
