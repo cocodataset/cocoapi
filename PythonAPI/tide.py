c4e94a9b5b174726059e3e49b262d7b772e560b3
dh <jinmingteo95@gmail.com>
2021-05-18 12:01:59 +0800

    [TIDE] add in argparse option for neater code

--- PythonAPI/tide.py
+++ PythonAPI/tide.py
@@ -1,8 +1,18 @@
+import argparse
+from pathlib import Path
 from tidecv import TIDE, datasets
 
-gt = datasets.COCO('test_vehicle.json')
+parser = argparse.ArgumentParser()
+parser.add_argument('GTfile')
+parser.add_argument('DTfile')
+args = parser.parse_args()
+annFile = Path(args.GTfile)
+resFile = Path(args.DTfile)
+assert annFile.is_file()
+assert resFile.is_file()
 
-bbox_results = datasets.COCOResult('ans_vehicle.json')
+gt = datasets.COCO(annFile)
+bbox_results = datasets.COCOResult(resFile)
 
 tide = TIDE()
 tide.evaluate(gt, bbox_results, mode=TIDE.OBB)
